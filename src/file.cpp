6a16a1bdaee0c01e4fec28bb34d2191a16ff4d2c
Emily Shepherd <emily@emilyshepherd.me>
2015-11-18 22:19:07 +0000

    FIXED POINT YALL!

---
+++ src/file.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int classify(int* v);
+
+void load(char* path)
+{
+    FILE * file = fopen(path, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "[ERROR] Impossible to open object file: '%s'!\n", path);
+        return;
+    }
+
+    int* data = new int[305];
+    float success = 0;
+    float total   = 0;
+    int i = 0;
+
+    while (!feof(file))
+    {
+        fscanf(file, "%d,", &data[i++]);
+
+        if (i == 300)
+        {
+            total++;
+            int expected;
+            int got = classify(data);
+
+            fscanf(file, "%d,\n", &expected);
+            i = 0;
+            printf("%d %d\n", got, got == expected);
+
+            success += (got==expected);
+        }
+    }
+
+    printf("%f\n", success / total * 100);
+}
